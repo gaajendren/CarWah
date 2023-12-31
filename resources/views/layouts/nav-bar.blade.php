8ecf8e27b1385dc18be6cb3d0696e630ecff4547
gaajendren <gaajendren123@gmail.com>
2023-12-31 23:15:04 +0800

    hi

---
+++ resources/views/layouts/nav-bar.blade.php
@@ -0,0 +1,25 @@
+<div class="nav-bar blue">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+                    <a href="#" class="navbar-brand">MENU</a>
+                    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse justify-content-between hd-container" id="navbarCollapse">
+                        <div class="navbar-nav mr-auto hd-link">
+                            <a href="index.html" class="nav-item nav-link active">Home</a>
+                            <a href="about.html" class="nav-item nav-link">About</a>
+                            <a href="service.html" class="nav-item nav-link">Service</a>
+                            <a href="price.html" class="nav-item nav-link">Price</a>
+                            <a href="location.html" class="nav-item nav-link">Washing Points</a>
+                            
+                            <a href="contact.html" class="nav-item nav-link">Contact</a>
+                        </div>
+                        <div class="ml-auto">
+                            <a class="btn btn-custom" id="btn" href="#">Get Appointment</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
